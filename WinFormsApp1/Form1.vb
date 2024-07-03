4286e96d185d07a3eb2492b9da1471fe0b0f4782
Mitchell Vetters <999463@stpetersgore.school.nz>
2024-07-04 10:44:25 +1200

    added desciptions to most of the code after descriptions are added to all the code i will screenshot and add to my word doc and work on that form 1 done half of form 2 done form 3 to do

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -1,95 +1,82 @@
 ﻿Public Class Form1
     Public customername As String = "" 'dewfines text enter as customer name 
     Public customeradress As String = "" ' defines tect entered in adress box as customer adress
-    Public deliverycharge As Decimal = 3
-    Public delivery As Decimal = 0
-    Dim phonenumber As String = "" ' defines text entered in the phone box as hine bx
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    End Sub
-
+    Public deliverycharge As Decimal = 3 'sets the delvery fee to three dollars
+    Public delivery As Decimal = 0 ' sets delivery to zero to have a separte system to pick up and delivery
+    Dim phonenumber As String = "" ' defines numbers entered in the phone box as text
     Private Sub Btndelivery_Click(sender As Object, e As EventArgs) Handles Btndelivery.Click
         'shows adress box
-        delivery = deliverycharge
-        Lbladress.Show()
-        Txtaddress.Show()
-
+        delivery = deliverycharge 'sets the deivery to three dollars 
+        Lbladress.Show() ' shows the label for the adress 
+        Txtaddress.Show() ' shows the text box for the adress
     End Sub
 
     Private Sub Btnconfirm_Click(sender As Object, e As EventArgs) Handles Btnconfirm.Click
-        customername = Txtname.Text.Trim
-        phonenumber = mtbphone.Text.Trim 'removes leading spaces
-        customeradress = Txtaddress.Text.Trim
-        phonenumber = phonenumber.Remove(0, 1) 'removes brakets and dashes from phone number
-        phonenumber = phonenumber.Remove(3, 1)
-        phonenumber = phonenumber.Remove(3, 1)
-        phonenumber = phonenumber.Remove(6, 1)
-        phonenumber = Val(phonenumber)
-        If Len(phonenumber) < 8 Then
-            MessageBox.Show("please enter valid Phone number")
+        customername = Txtname.Text.Trim ' sets the customers name to a trimmed text removng spaces
+        phonenumber = mtbphone.Text.Trim 'removes leading spaces in the phone number
+        customeradress = Txtaddress.Text.Trim 'sets the customers adress as a trimed text removing spaces
+        phonenumber = phonenumber.Remove(0, 1) 'removes first bracket from the masked text box
+        phonenumber = phonenumber.Remove(3, 1) 'remves a bracket from the masked text box
+        phonenumber = phonenumber.Remove(3, 1) ' removes one of the dsashes from the masked text box
+        phonenumber = phonenumber.Remove(6, 1) ' removes the last of the dashes from the masked text box
+        phonenumber = Val(phonenumber) ' sets the phone number from text to values/numbers
+        If Len(phonenumber) < 8 Then 'if thers benn numbers enterd but under 8 it displays this 
+            MessageBox.Show("please enter valid Phone number") ' tells the person entering details that the phone numbere is not valid
         Else
-            If delivery > 0 Then fordelivery() Else forcollection()
+            If delivery > 0 Then fordelivery() Else forcollection() 'other code is pulled up for each sub
         End If
 
     End Sub
 
 
     Private Sub Btnno_Click(sender As Object, e As EventArgs) Handles Btnno.Click
-        Lblyousure.Hide()
-        Btnyes.Hide()
-        Btnno.Hide()
+        Lblyousure.Hide() 'if no was clicked it removes the lable asking if their sure 
+        Btnyes.Hide() ' hides the yes button from the form
+        Btnno.Hide() ' hides the no button from the form
     End Sub
 
     Private Sub Btnpickup_Click(sender As Object, e As EventArgs) Handles Btnpickup.Click
-        'adress hides
-        delivery = 0
-        Lbladress.Hide()
-        Txtaddress.Hide()
+        delivery = 0 'sets the delivery to 0
+        Lbladress.Hide() ' hides the aadress label from the form
+        Txtaddress.Hide() ' hides the address text box from the form
     End Sub
 
     Private Sub Btnyes_Click(sender As Object, e As EventArgs) Handles Btnyes.Click
-        Form2.Show()
-        Me.Hide()
+        Form2.Show() 'shows form two
+        Me.Hide() ' hides this form
     End Sub
 
-    Private Sub Txtname_TextChanged(sender As Object, e As EventArgs) Handles Txtname.TextChanged
-
-    End Sub
-
-    Private Sub Lblname_Click(sender As Object, e As EventArgs) Handles Lblname.Click
-
-    End Sub
-
-    Public Sub fordelivery()
+    Public Sub fordelivery() ' if the delivery charge is more then zero codes is rederiv=cted to this sub
         If customername = "" Then
-            MessageBox.Show("please enter name")
+            MessageBox.Show("please enter name") 'if nothing is entered into the name text this will be displayed
         ElseIf phonenumber = 0 Then
-            MessageBox.Show("please enter valid phone number")
+            MessageBox.Show("please enter valid phone number") ' if nothing is entered ibnto the phone number text box this will be displayed
+
         ElseIf customeradress = "" Then
-            MessageBox.Show("please enter address")
-            'nice
+            MessageBox.Show("please enter address") ' if nothing is entered into the address text boxt his is displayed
         Else
-            Lblyousure.Show()
-            Btnyes.Show()
-            Btnno.Show()
+            Lblyousure.Show() 'shows label asking if customer is sure that their details are correct
+            Btnyes.Show() ' shows button to move to the next form
+            Btnno.Show() ' shows button to hide these last two 
         End If
     End Sub
-    Public Sub forcollection()
+    Public Sub forcollection() ' if delivery is sero than code is redirected to this sub
         If customername = "" Then
-            MessageBox.Show("please enter name")
+            MessageBox.Show("please enter name") 'if nothing is entered into the customer name text box this is displayed
         ElseIf phonenumber = 0 Then
-            MessageBox.Show("please enter valid phone number")
+            MessageBox.Show("please enter valid phone number") ' if nothing is entered into the phone number text box than this is displayed
         Else
-            Lblyousure.Show()
-            Btnyes.Show()
-            Btnno.Show()
+            Lblyousure.Show() 'shows label asking if the customer is sure that their details are correct
+            Btnyes.Show() 'shows the button to move to the next form
+            Btnno.Show() ' hshows the button that hides the other two
         End If
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Btnend.Click
-        End
+        End ' if the X button is clicked than the application ends and has to be reopened
     End Sub
 
     Private Sub Btnrestart_Click(sender As Object, e As EventArgs) Handles Btnrestart.Click
-        Application.Restart()
+        Application.Restart() ' if the restart bvutton is pressed it resets the application and removes everything entered 
     End Sub
 End Class
--- WinFormsApp1/Form2.vb
+++ WinFormsApp1/Form2.vb
@@ -1,78 +1,76 @@
 ﻿Public Class Form2
-    Public Const REGULARPRICE As Decimal = 8.5
-    Public Const GOURMETPREMIUM As Decimal = 5
-    Public Const GOURMETPRICE As Decimal = REGULARPRICE + GOURMETPREMIUM
-    Public pizzaarray(19, 3) As String
+    Public Const REGULARPRICE As Decimal = 8.5 ' sets the regular price to $8.5
+    Public Const GOURMETPREMIUM As Decimal = 5 ' sets the gourmet premium to $5
+    Public Const GOURMETPRICE As Decimal = REGULARPRICE + GOURMETPREMIUM ' sets the gourmet price to the regular plus the gourmet premuim
+    Public pizzaarray(19, 3) As String ' creates an array for the name,price,the number of pizzas and totals 
 
     Private Sub Btnback1_Click(sender As Object, e As EventArgs) Handles Btnback1.Click
-        Form1.Show()
-        Me.Hide()
+        Form1.Show() ' if the back button is clicked it brings up form one with all the details still entered
+        Me.Hide() ' hides this form to show form one
     End Sub
 
     Private Sub Btnrestart_Click(sender As Object, e As EventArgs) Handles Btnrestart.Click
-        Application.Restart()
+        Application.Restart() ' if the restart button is clicked it restarts the application to form one with nothing entered
 
     End Sub
 
     Private Sub Btnclose_Click(sender As Object, e As EventArgs) Handles Btnclose.Click
-        End
+        End ' if the X button is clicked than the application ends and has to be reopened
     End Sub
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'stores the name of the regular pizza's
-        pizzaarray(0, 0) = "Cheese Pizza"
-        pizzaarray(1, 0) = "hawaiian pizza"
-        pizzaarray(2, 0) = "pepperoni pizza"
-        pizzaarray(3, 0) = "meat lovers pizza"
-        pizzaarray(4, 0) = "cheesy garlic pizza"
-        pizzaarray(5, 0) = "veggie pizza"
-        pizzaarray(6, 0) = "the Godfather"
-        pizzaarray(7, 0) = "butter chicken"
-        pizzaarray(8, 0) = "mr wedge"
-        pizzaarray(9, 0) = "spicy pepper"
+        pizzaarray(0, 0) = "Cheese Pizza" ' sets the name of the first pizza to chesse pizza in the array
+        pizzaarray(1, 0) = "hawaiian pizza" 'sets the name of the second pizza to hawaiian pizza in the array
+        pizzaarray(2, 0) = "pepperoni pizza" 'sets the name fo the third pizza to pepperoni pizza in the array
+        pizzaarray(3, 0) = "meat lovers pizza" ' sets the fourth pizza to meat lovers pizza in the array
+        pizzaarray(4, 0) = "cheesy garlic pizza" ' sets the fifth pizza to chessy garlic pizza in the array 
+        pizzaarray(5, 0) = "veggie pizza" ' sets the sixth pizza to veggie pizza in the array
+        pizzaarray(6, 0) = "the Godfather" ' sets the seventh pizza to the godfahter in the array
+        pizzaarray(7, 0) = "butter chicken" ' sets the eighth pizza to butter chicken in the array
+        pizzaarray(8, 0) = "mr wedge" ' sets the ninth pizza to mr wedge in they array
+        pizzaarray(9, 0) = "spicy pepper" 'sets the tenth pizza to spicy pepper in the array 
 
-        'sets the labels as the names above
-        Lblpizza1.Text = pizzaarray(0, 0)
-        lblpizza2.Text = pizzaarray(1, 0)
-        lblpizza3.Text = pizzaarray(2, 0)
-        lblpizza4.Text = pizzaarray(3, 0)
-        Lblpizza5.Text = pizzaarray(4, 0)
-        Lblpizza6.Text = pizzaarray(5, 0)
-        Lblpizza7.Text = pizzaarray(6, 0)
-        Lblpizza8.Text = pizzaarray(7, 0)
-        Lblpizza9.Text = pizzaarray(8, 0)
-        Lblpizza10.Text = pizzaarray(9, 0)
+        Lblpizza1.Text = pizzaarray(0, 0) ' changes the name in the text box to the corresponding slot in the array
+        lblpizza2.Text = pizzaarray(1, 0) ' changes the name in the text box to the corresponding slot in the array
+        lblpizza3.Text = pizzaarray(2, 0) ' changes the name in the text box to the corresponding slot in the array
+        lblpizza4.Text = pizzaarray(3, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza5.Text = pizzaarray(4, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza6.Text = pizzaarray(5, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza7.Text = pizzaarray(6, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza8.Text = pizzaarray(7, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza9.Text = pizzaarray(8, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza10.Text = pizzaarray(9, 0) ' changes the name in the text box to the corresponding slot in the array
 
-        'stores the prices
         For x = 0 To 9
-            pizzaarray(x, 1) = REGULARPRICE
+            pizzaarray(x, 1) = REGULARPRICE ' sets the price for regular pizzads in the correct slot in the array
         Next
-        Lblprice.Text = Val(pizzaarray(0, 1)).ToString("C")
+        Lblprice.Text = Val(pizzaarray(0, 1)).ToString("C") 'sets the price label to the price in the corresponding slot in the array
 
-        pizzaarray(10, 0) = "GREEK LAMB TZATZIKI"
-        pizzaarray(11, 0) = "LAMB MEATLOVERS"
-        pizzaarray(12, 0) = "CHICKEN DELUXE"
-        pizzaarray(13, 0) = "BBQ PORK BELLY"
-        pizzaarray(14, 0) = "THE LOT"
-        pizzaarray(15, 0) = "BUFFALO CHICKEN"
-        pizzaarray(16, 0) = "PERI-PERI CHICKEN"
-        pizzaarray(17, 0) = "GARLIC PRAWN"
-        pizzaarray(18, 0) = "APRICOT CHICKEN"
-        pizzaarray(19, 0) = "MEGA MEATLOVERS"
+        pizzaarray(10, 0) = "GREEK LAMB TZATZIKI" ' sets the eleventh pizza to greek lamb tzarziki in the array
+        pizzaarray(11, 0) = "LAMB MEATLOVERS" ' sets the twelevth pizza to lamb meatlovers in the arrray
+        pizzaarray(12, 0) = "CHICKEN DELUXE" ' sets the thirtenth pizza to chicken deluxe in the array 
+        pizzaarray(13, 0) = "BBQ PORK BELLY" ' sets the fourtenth pizza to bbq pork belly 
+        pizzaarray(14, 0) = "THE LOT" ' sets the fifthtenth pizza to the lot 
+        pizzaarray(15, 0) = "BUFFALO CHICKEN" ' sets the sixthenth pizza to buffalo chicken
+        pizzaarray(16, 0) = "PERI-PERI CHICKEN" ' sets the seventhenth pizza to peri peri chicken in the array
+        pizzaarray(17, 0) = "GARLIC PRAWN" 'sets the eightenth pizza to garlic prawn in the array
+        pizzaarray(18, 0) = "APRICOT CHICKEN" ' sets the ninetenth pizza to apricot chicken in the array
+        pizzaarray(19, 0) = "MEGA MEATLOVERS" ' sets the twenteth pizza to mega meatlovers in to array
 
-        Lblpizza11.Text = pizzaarray(10, 0)
-        Lblpizza12.Text = pizzaarray(11, 0)
-        Lblpizza13.Text = pizzaarray(12, 0)
-        Lblpizza14.Text = pizzaarray(13, 0)
-        Lblpizza15.Text = pizzaarray(14, 0)
-        lblpizza16.Text = pizzaarray(15, 0)
-        Lblpizza17.Text = pizzaarray(16, 0)
-        Lblpizza18.Text = pizzaarray(17, 0)
-        Lblpizza19.Text = pizzaarray(18, 0)
-        Lblpizza20.Text = pizzaarray(19, 0)
+        Lblpizza11.Text = pizzaarray(10, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza12.Text = pizzaarray(11, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza13.Text = pizzaarray(12, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza14.Text = pizzaarray(13, 0) '  changes the name in the text box to the corresponding slot in the array
+        Lblpizza15.Text = pizzaarray(14, 0) ' changes the name in the text box to the corresponding slot in the array
+        lblpizza16.Text = pizzaarray(15, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza17.Text = pizzaarray(16, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza18.Text = pizzaarray(17, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza19.Text = pizzaarray(18, 0) ' changes the name in the text box to the corresponding slot in the array
+        Lblpizza20.Text = pizzaarray(19, 0) ' changes the name in the text box to the corresponding slot in the array
 
         For x = 10 To 19
-            pizzaarray(x, 1) = GOURMETPRICE
+            pizzaarray(x, 1) = GOURMETPRICE ' sets the gourmet price to the corresponding spot in the array
         Next
         Lblgourmetprice.Text = Val(pizzaarray(10, 1)).ToString("C")
 
